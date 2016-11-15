4caf1aa11f497773921a8cf00460464055df3327
Mike Houston <mike@kothar.net>
2016-11-15 23:27:13 +0000

    Release version bump to 1.1.0

--- src/main/java/net/kothar/csview/cocoa/MacLoader.java
+++ src/main/java/net/kothar/csview/cocoa/MacLoader.java
@@ -42,7 +42,7 @@ import net.kothar.csview.Menus;
 public class MacLoader {
 
 	static final String APP_NAME = "CSView";
-	static final String VERSION = "1.0.6";
+	static final String VERSION = "1.1.0";
 
 	private static Display display;
 
