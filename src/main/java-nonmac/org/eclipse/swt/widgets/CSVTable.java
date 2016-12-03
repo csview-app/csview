2341134835322dc5ed8f0fd5facbcf8bfd96b811
Mike Houston <mike@kothar.net>
2016-12-03 00:10:11 +0000

    Remove SWT Table override on windows - not needed

--- src/main/java-nonmac/org/eclipse/swt/widgets/CSVTable.java
+++ src/main/java-nonmac/org/eclipse/swt/widgets/CSVTable.java
@@ -1,24 +1,9 @@
 package org.eclipse.swt.widgets;
 
-import org.eclipse.swt.SWT;
-
 public class CSVTable extends Table {
 
 	public CSVTable(Composite parent, int style) {
 		super(parent, style);
-	}
-	
-	@Override
-	public void clear(int index) {
-		checkWidget ();
-		if (!(0 <= index && index < items.length)) error (SWT.ERROR_INVALID_RANGE);
-		TableItem item = items [index];
-		if (item != null) {
-			if (currentItem != item) {
-				items[index].dispose();
-				items[index] = null;
-			}
-		}
 	}
 
 }
