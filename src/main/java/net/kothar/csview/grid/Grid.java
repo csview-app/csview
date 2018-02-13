c53d8612fea2c6f981b87d240574e700ac1d1b4f
Mike Houston <mike@kothar.net>
2018-02-13 23:18:31 +0000

    Updated SWT dependencies to latest available on maven (MacOS)

--- src/main/java/net/kothar/csview/grid/Grid.java
+++ src/main/java/net/kothar/csview/grid/Grid.java
@@ -40,43 +40,41 @@ import net.kothar.csview.adt.SizeTree;
 
 public class Grid extends Composite {
 
-	public static final int DEFAULT = -1;
-	static final int SCROLL_FACTOR = 4;
-	static final int TILE_ROWS = 10;
+	public static final int	DEFAULT			= -1;
+	static final int		SCROLL_FACTOR	= 4;
+	static final int		TILE_ROWS		= 10;
 
 	Canvas canvas;
 
-	private int horizontalCellPadding = 5;
-	private int verticalCellPadding = 2;
+	private int	horizontalCellPadding	= 5;
+	private int	verticalCellPadding		= 2;
 
-	SizeTree rows = new SizeTree(getColumnHeaderSize());
-	SizeTree cols = new SizeTree(getRowHeaderSize());
+	SizeTree	rows	= new SizeTree(getColumnHeaderSize());
+	SizeTree	cols	= new SizeTree(getRowHeaderSize());
 
-	private IGridContentProvider contentProvider;
-	private ITableLabelProvider labelProvider;
-	ILabelProvider rowLabelProvider;
-	ILabelProvider colLabelProvider;
-	
+	private IGridContentProvider	contentProvider;
+	private ITableLabelProvider		labelProvider;
+	ILabelProvider					rowLabelProvider;
+	ILabelProvider					colLabelProvider;
+
 	Selection selection = new Selection();
 
-	private Integer columnHeaderSize;
-	private Integer rowHeaderSize;
+	private Integer	columnHeaderSize;
+	private Integer	rowHeaderSize;
 
-	Cache<Point, String> labelCache = 
-			CacheBuilder.newBuilder()
-			.maximumSize(10_000)
-			.build();
+	Cache<Point, String> labelCache = CacheBuilder.newBuilder()
+		.maximumSize(10_000)
+		.build();
 
-	Cache<Point, Image> tileCache = 
-			CacheBuilder.newBuilder()
-			.maximumSize(1000)
-			.removalListener((RemovalNotification<Point, Image> e) -> e.getValue().dispose())
-			.build();
+	Cache<Point, Image> tileCache = CacheBuilder.newBuilder()
+		.maximumSize(1000)
+		.removalListener((RemovalNotification<Point, Image> e) -> e.getValue().dispose())
+		.build();
 
-	private MouseHandler mouseHandler;
-	private Image nullTile;
-	private Point currentCell;
-	
+	private MouseHandler	mouseHandler;
+	private Image			nullTile;
+	private Point			currentCell;
+
 	private List<CellListener> currentCellListeners = new ArrayList<>();
 
 	public Grid(Composite parent, int style) {
@@ -85,7 +83,7 @@ public class Grid extends Composite {
 		cols.setCount(1);
 		rows.setCount(1);
 		currentCell = new Point(0, 0);
-		
+
 		setLayout(new FillLayout());
 		createContents(this);
 	}
@@ -116,7 +114,7 @@ public class Grid extends Composite {
 
 		// Hook mouse handling
 		mouseHandler = createMouseHandler();
-		
+
 		KeyboardHandler keyListener = new KeyboardHandler(this);
 		canvas.addKeyListener(keyListener);
 	}
@@ -135,7 +133,7 @@ public class Grid extends Composite {
 		if (cols.getTotal() > 0)
 			paintColHeaders(gc);
 		paintCorner(gc);
-		
+
 		// Render border with bottom of control
 		gc.setForeground(gc.getDevice().getSystemColor(SWT.COLOR_WIDGET_HIGHLIGHT_SHADOW));
 		Rectangle bounds = getBounds();
@@ -145,7 +143,7 @@ public class Grid extends Composite {
 	}
 
 	private void paintCells(GC gc) {
-		
+
 		int xOffset = getXOffset();
 		int yOffset = getYOffset();
 
@@ -153,25 +151,25 @@ public class Grid extends Composite {
 		int firstX = cols.getPosition(firstCol);
 		int firstRow = yOffset < rows.getTotal() ? rows.getItemAt(yOffset) : rows.getCount() - 1;
 		firstRow -= firstRow % TILE_ROWS;
-		
+
 		int firstY = rows.getPosition(firstRow);
 		int colCount = cols.getCount();
 		int rowCount = rows.getCount();
-		
+
 		int xAdjust = xOffset - firstX;
 		int yAdjust = yOffset - firstY;
 
 		int columnHeaderSize = getColumnHeaderSize();
 		int rowHeaderSize = getRowHeaderSize();
-		
+
 		int height = canvas.getBounds().height;
 		int width = canvas.getBounds().width;
 		Device device = gc.getDevice();
 		getNullTile(device);
-		
+
 		int row = firstRow;
 		for (int y = 0; row < rowCount && y + columnHeaderSize - yAdjust < height;) {
-			
+
 			int col = firstCol;
 			int tileHeight = 0;
 			for (int x = 0; col < colCount && x + rowHeaderSize - xAdjust < width;) {
@@ -190,7 +188,7 @@ public class Grid extends Composite {
 					e.printStackTrace();
 				}
 			}
-			
+
 			y += tileHeight;
 			row += TILE_ROWS;
 		}
@@ -226,18 +224,18 @@ public class Grid extends Composite {
 	private Image renderTile(Device device, Point position) {
 		try {
 			int width = cols.getSize(position.x);
-			
+
 			int lastY = rows.getTotal();
 			if (position.y + TILE_ROWS < rows.getCount())
 				lastY = rows.getPosition(position.y + TILE_ROWS);
 			int height = lastY - rows.getPosition(position.y);
-			
+
 			if (width == 0 || height == 0)
 				return nullTile;
-			
+
 			Image tile = new Image(device, width, height);
 			GC gc = new GC(tile);
-			
+
 			renderBackground(gc, tile.getBounds());
 
 			int row = position.y;
@@ -266,7 +264,7 @@ public class Grid extends Composite {
 	private void renderCurrentCell(GC gc, int y, Rectangle viewport) {
 		if (currentCell.x == viewport.x && currentCell.y == viewport.y) {
 			gc.setForeground(gc.getDevice().getSystemColor(SWT.COLOR_WIDGET_NORMAL_SHADOW));
-			gc.drawRectangle(0, y, viewport.width-1, viewport.height-1);
+			gc.drawRectangle(0, y, viewport.width - 1, viewport.height - 1);
 		}
 	}
 
@@ -280,7 +278,7 @@ public class Grid extends Composite {
 	private void renderSelection(GC gc, int y, Rectangle viewport) {
 		if (!selection.isSelected(viewport.x, viewport.y))
 			return;
-		
+
 		gc.setBackground(gc.getDevice().getSystemColor(SWT.COLOR_LIST_SELECTION));
 		gc.fillRectangle(0, y, viewport.width, viewport.height);
 	}
@@ -288,17 +286,17 @@ public class Grid extends Composite {
 	private void paintCorner(GC gc) {
 		gc.setBackground(gc.getDevice().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
 		gc.setForeground(gc.getDevice().getSystemColor(SWT.COLOR_WIDGET_NORMAL_SHADOW));
-		
+
 		int rowHeaderSize = getRowHeaderSize();
 		int columnHeaderSize = getColumnHeaderSize();
-		
+
 		gc.fillRectangle(0, 0, rowHeaderSize, columnHeaderSize);
-		gc.drawLine(0, columnHeaderSize-1, rowHeaderSize-1, columnHeaderSize-1);
-		gc.drawLine(rowHeaderSize-1, 0, rowHeaderSize-1, columnHeaderSize-1);
-		
+		gc.drawLine(0, columnHeaderSize - 1, rowHeaderSize - 1, columnHeaderSize - 1);
+		gc.drawLine(rowHeaderSize - 1, 0, rowHeaderSize - 1, columnHeaderSize - 1);
+
 		gc.setForeground(gc.getDevice().getSystemColor(SWT.COLOR_WIDGET_HIGHLIGHT_SHADOW));
-		gc.drawLine(0, columnHeaderSize, rowHeaderSize-1, columnHeaderSize);
-		gc.drawLine(rowHeaderSize, 0, rowHeaderSize, columnHeaderSize-1);
+		gc.drawLine(0, columnHeaderSize, rowHeaderSize - 1, columnHeaderSize);
+		gc.drawLine(rowHeaderSize, 0, rowHeaderSize, columnHeaderSize - 1);
 	}
 
 	private void paintRowHeaders(GC gc) {
@@ -314,14 +312,14 @@ public class Grid extends Composite {
 		Color textColor = device.getSystemColor(SWT.COLOR_WIDGET_FOREGROUND);
 		Color borderColor = device.getSystemColor(SWT.COLOR_WIDGET_NORMAL_SHADOW);
 		Color borderColor2 = device.getSystemColor(SWT.COLOR_WIDGET_HIGHLIGHT_SHADOW);
-		
+
 		gc.setBackground(backgroundColor);
 		gc.fillRectangle(0, columnHeaderSize, rowHeaderSize, canvas.getBounds().height - columnHeaderSize);
 
 		int yOffset = getYOffset();
 
 		gc.setClipping(0, 0, rowHeaderSize, bounds.height);
-		
+
 		int startRow;
 		if (rows.getTotal() < yOffset) {
 			startRow = rows.getCount() - 1;
@@ -329,7 +327,7 @@ public class Grid extends Composite {
 			startRow = rows.getItemAt(yOffset);
 		}
 		int y = columnHeaderSize + rows.getPosition(startRow) - yOffset;
-		
+
 		for (int i = startRow; i < rows.getCount(); i++) {
 			int height = rows.getSize(i);
 
@@ -337,12 +335,12 @@ public class Grid extends Composite {
 				break;
 			if (y < bounds.y)
 				continue;
-			
+
 			// Shadow header if row contains current cell
 			if (currentCell != null && currentCell.y == i) {
 				gc.setBackground(borderColor);
 				gc.setForeground(borderColor2);
-				
+
 				gc.fillRectangle(0, y, rowHeaderSize, height);
 			} else {
 				gc.setBackground(backgroundColor);
@@ -361,34 +359,34 @@ public class Grid extends Composite {
 
 			// Draw cell border
 			gc.setForeground(borderColor);
-			gc.drawLine(0, y-1, rowHeaderSize-1, y-1);
+			gc.drawLine(0, y - 1, rowHeaderSize - 1, y - 1);
 			gc.setForeground(borderColor2);
-			gc.drawLine(0, y, rowHeaderSize-1, y);
-			
+			gc.drawLine(0, y, rowHeaderSize - 1, y);
+
 			y += height;
 		}
 
 		// Right of row headers
 		gc.setForeground(borderColor);
-		gc.drawLine(rowHeaderSize-1, columnHeaderSize, rowHeaderSize-1, bounds.height);
-		
+		gc.drawLine(rowHeaderSize - 1, columnHeaderSize, rowHeaderSize - 1, bounds.height);
+
 		// Last row
 		int bottomEdge = rows.getTotal() - yOffset + columnHeaderSize;
 		if (bottomEdge < bounds.height) {
 			gc.drawLine(0, bottomEdge, rowHeaderSize, bottomEdge);
 		}
-		
+
 		// Highlight to left of row headers
 		gc.setForeground(borderColor2);
 		gc.drawLine(0, 0, 0, bounds.height);
-		
+
 		gc.setClipping((Rectangle) null);
 	}
 
 	public int getYOffset() {
 		return canvas.getVerticalBar().getSelection() * SCROLL_FACTOR;
 	}
-	
+
 	public void setYOffset(int yOffset) {
 		canvas.getVerticalBar().setSelection(yOffset / SCROLL_FACTOR);
 	}
@@ -405,7 +403,7 @@ public class Grid extends Composite {
 		Color textColor = gc.getDevice().getSystemColor(SWT.COLOR_WIDGET_FOREGROUND);
 		Color borderColor = gc.getDevice().getSystemColor(SWT.COLOR_WIDGET_NORMAL_SHADOW);
 		Color borderColor2 = gc.getDevice().getSystemColor(SWT.COLOR_WIDGET_HIGHLIGHT_SHADOW);
-		
+
 		gc.setBackground(backgroundColor);
 		gc.fillRectangle(rowHeaderSize, 0, canvas.getBounds().width - rowHeaderSize, columnHeaderSize);
 
@@ -423,13 +421,13 @@ public class Grid extends Composite {
 			if (currentCell != null && currentCell.x == i) {
 				gc.setBackground(borderColor);
 				gc.setForeground(borderColor2);
-				
+
 				gc.fillRectangle(x, 0, width, columnHeaderSize);
 			} else {
 				gc.setBackground(backgroundColor);
 				gc.setForeground(textColor);
 			}
-			
+
 			// Render label
 			String text;
 			if (colLabelProvider != null) {
@@ -441,24 +439,24 @@ public class Grid extends Composite {
 			gc.setClipping(x, 0, width, columnHeaderSize);
 			gc.drawString(text, x + horizontalCellPadding, verticalCellPadding);
 			gc.setClipping((Rectangle) null);
-			
+
 			// Render border between columns
 			gc.setForeground(borderColor);
-			gc.drawLine(x-1, 0, x-1, columnHeaderSize-1);
+			gc.drawLine(x - 1, 0, x - 1, columnHeaderSize - 1);
 			gc.setForeground(borderColor2);
-			gc.drawLine(x, 0, x, columnHeaderSize-1);
+			gc.drawLine(x, 0, x, columnHeaderSize - 1);
 		}
 
 		// Bottom of header
 		gc.setForeground(borderColor);
-		gc.drawLine(rowHeaderSize, columnHeaderSize-1, bounds.width, columnHeaderSize-1);
-		
+		gc.drawLine(rowHeaderSize, columnHeaderSize - 1, bounds.width, columnHeaderSize - 1);
+
 		// Last column
 		int rightEdge = cols.getTotal() - xOffset + rowHeaderSize;
 		if (rightEdge < bounds.width) {
 			gc.drawLine(rightEdge, 0, rightEdge, columnHeaderSize);
 		}
-		
+
 		// Top highlight
 		gc.setForeground(borderColor2);
 		gc.drawLine(0, 0, bounds.width, 0);
@@ -467,7 +465,7 @@ public class Grid extends Composite {
 	public int getXOffset() {
 		return canvas.getHorizontalBar().getSelection() * SCROLL_FACTOR;
 	}
-	
+
 	public void setXOffset(int xOffset) {
 		canvas.getHorizontalBar().setSelection(xOffset / SCROLL_FACTOR);
 	}
@@ -475,9 +473,9 @@ public class Grid extends Composite {
 	private void renderGridlines(GC gc, int y, Rectangle viewport) {
 
 		gc.setForeground(gc.getDevice().getSystemColor(SWT.COLOR_WIDGET_LIGHT_SHADOW));
-		gc.setLineDash(new int[] {2,2});
-		
-		gc.drawLine(0, y + viewport.height-1, viewport.width - 1, y + viewport.height-1);
+		gc.setLineDash(new int[] { 2, 2 });
+
+		gc.drawLine(0, y + viewport.height - 1, viewport.width - 1, y + viewport.height - 1);
 		gc.drawLine(viewport.width - 1, y, viewport.width - 1, y + viewport.height - 1);
 
 		gc.setLineStyle(SWT.LINE_SOLID);
@@ -503,12 +501,12 @@ public class Grid extends Composite {
 		if (isDisposed()) {
 			return;
 		}
-		
+
 		int max = getTotalHeight() - canvas.getBounds().height;
 		ScrollBar bar = canvas.getVerticalBar();
 		if (max > 0) {
 			bar.setVisible(true);
-			int total = (max + canvas.getHorizontalBar().getSize().y + getColumnHeaderSize())/SCROLL_FACTOR + 11;
+			int total = (max + canvas.getHorizontalBar().getSize().y + getColumnHeaderSize()) / SCROLL_FACTOR + 11;
 			bar.setMaximum(total);
 			bar.setEnabled(true);
 		} else {
@@ -556,12 +554,12 @@ public class Grid extends Composite {
 		if (isDisposed()) {
 			return;
 		}
-		
+
 		int max = getTotalWidth() - canvas.getBounds().width;
 		ScrollBar bar = canvas.getHorizontalBar();
 		if (max > 0) {
 			bar.setVisible(true);
-			int total = (max + canvas.getVerticalBar().getSize().x + getRowHeaderSize())/SCROLL_FACTOR + 11;
+			int total = (max + canvas.getVerticalBar().getSize().x + getRowHeaderSize()) / SCROLL_FACTOR + 11;
 			bar.setMaximum(total);
 			bar.setEnabled(true);
 		} else {
@@ -640,7 +638,7 @@ public class Grid extends Composite {
 	public void setRows(int count) {
 		int oldCount = rows.getCount();
 		rows.setCount(count);
-		
+
 		int minCount = Math.min(count, oldCount);
 		int invalidateAfter = minCount - minCount % TILE_ROWS;
 		if (invalidateAfter < count) {
@@ -648,24 +646,24 @@ public class Grid extends Composite {
 			labelCache.asMap().keySet().removeIf(p -> p.y >= invalidateAfter);
 		}
 		updateVerticalScroll();
-		
+
 		canvas.redraw();
 	}
 
 	public void setHeaderVisible(boolean headerVisible) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void setLinesVisible(boolean linesVisible) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void setColumnSize(int column, int newSize) {
 		cols.setSize(column, newSize);
 		updateHorizontalScroll();
-		
+
 		tileCache.asMap().keySet().removeIf(p -> p.x == column);
 		canvas.redraw();
 	}
@@ -685,7 +683,7 @@ public class Grid extends Composite {
 			return "";
 		}
 	}
-	
+
 	public Selection getSelection() {
 		return selection;
 	}
@@ -708,22 +706,22 @@ public class Grid extends Composite {
 		if (!getCellBounds().contains(cell)) {
 			return;
 		}
-		
+
 		if (currentCell != null) {
 			invalidateTiles(new Rectangle(currentCell.x, currentCell.y, 1, 1));
 		}
 		currentCell = cell;
-		
+
 		int x = cols.getPosition(cell.x);
 		int y = rows.getPosition(cell.y);
 		int width = cols.getSize(cell.x);
 		int height = rows.getSize(cell.y);
-		
+
 		invalidateTiles(new Rectangle(cell.x, cell.y, 1, 1));
-		
+
 		// Adjust scroll position to ensure current cell is visible
 		int xOffset = getXOffset();
-		if (x + width/2 < xOffset) {
+		if (x + width / 2 < xOffset) {
 			setXOffset(x);
 		} else {
 			int canvasWidth = canvas.getBounds().width;
@@ -743,28 +741,28 @@ public class Grid extends Composite {
 				setYOffset(y - canvasHeight + columnHeaderSize + height + 16);
 			}
 		}
-		
+
 		currentCellListeners.forEach(l -> l.notify(currentCell));
 	}
 
 	public Rectangle getCellBounds() {
 		return new Rectangle(0, 0, cols.getCount(), rows.getCount());
 	}
-	
+
 	void invalidateTiles(Rectangle... regions) {
 		invalidateTiles(Arrays.asList(regions));
 	}
-	
+
 	void invalidateTiles(Iterable<Rectangle> regions) {
 		tileCache.asMap().keySet().removeIf(p -> {
 			Rectangle tileRegion = new Rectangle(p.x, p.y, 1, TILE_ROWS);
-			for (Rectangle r: regions) {
+			for (Rectangle r : regions) {
 				if (r.intersects(tileRegion))
 					return true;
 			}
 			return false;
 		});
-		
+
 		canvas.redraw();
 	}
 
@@ -774,7 +772,7 @@ public class Grid extends Composite {
 		if (bounds == null) {
 			return;
 		}
-		
+
 		String nl = System.getProperty("line.separator");
 
 		// Prepare data
@@ -785,7 +783,7 @@ public class Grid extends Composite {
 					String label = getLabel(col + bounds.x, row + bounds.y);
 					if (label.isEmpty())
 						continue;
-					
+
 					Integer size = colSizes.get(col);
 					if (size == null)
 						size = 0;
@@ -793,25 +791,26 @@ public class Grid extends Composite {
 				}
 			}
 		}
-		
+
 		StringBuilder text = new StringBuilder();
 		StringBuilder html = new StringBuilder("<table style=\"border: 1px solid #999;\" cellspacing=\"0\">" + nl);
-		
+
 		for (int row = 0; row < bounds.height; row++) {
 			html.append("<tr>" + nl);
 			for (int col = 0; col < bounds.width; col++) {
 				Integer size = colSizes.get(col);
 				if (size == null || size == 0)
 					continue;
-				
+
 				String label = "";
 				if (selection.isSelected(col + bounds.x, row + bounds.y))
 					label = getLabel(col + bounds.x, row + bounds.y);
-				
+
 				text.append(label);
-				html.append("<td style=\"border: 1px dotted #ddd; padding: 3px 5px;\">" + StringEscapeUtils.escapeHtml4(label) + "</td>" + nl);
-				
-				if (col < bounds.width -1) {
+				html.append("<td style=\"border: 1px dotted #ddd; padding: 3px 5px;\">"
+					+ StringEscapeUtils.escapeHtml4(label) + "</td>" + nl);
+
+				if (col < bounds.width - 1) {
 					text.append(Strings.padStart(" ", size - label.length() + 3, ' '));
 				}
 			}
@@ -819,13 +818,13 @@ public class Grid extends Composite {
 			html.append("</tr>" + nl);
 		}
 		html.append("</table>" + nl);
-		
+
 		Object[] data = new Object[] { text.toString(), html.toString() };
-		
+
 		TextTransfer textTransfer = TextTransfer.getInstance();
 		HTMLTransfer htmlTransfer = HTMLTransfer.getInstance();
 		Transfer[] transfers = new Transfer[] { textTransfer, htmlTransfer };
-		
+
 		Clipboard clipboard = new Clipboard(getDisplay());
 		clipboard.setContents(data, transfers);
 		clipboard.dispose();
