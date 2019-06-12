6fc5e8f15e6fa400951c3244608c7be33951933a
Mike Houston <mike@kothar.net>
2019-06-12 18:10:11 +0100

    Fix version number

--- src/main/java/net/kothar/csview/BaseLoader.java
+++ src/main/java/net/kothar/csview/BaseLoader.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 public abstract class BaseLoader implements ApplicationActions {
 
     public static final String APP_NAME = "CSView";
-    public static final String VERSION = "1.3.1";
+    public static final String VERSION = "1.3.2";
 
     protected Display display;
 
